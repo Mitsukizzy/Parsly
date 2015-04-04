080472b195f969dab9c1b02b7acaac9ab146cd2b
garland106 <garlandc@usc.edu>
2015-04-04 21:35:10 +0000

    we don't know what we're doing

--- resources/library/parsly.php
+++ resources/library/parsly.php
@@ -1,23 +1,35 @@
-<?php
-include_once("class.contextio.php");
+	<?php
+	include_once("class.contextio.php");
 
 
-//This function should clear out all tokens and accounts associated with our keys. 
-function flushTokens()
-{
-	$contextIO = new ContextIO('qd8cq03s','SogN0NW6RPJPkStv');
-	$listAccountTokensResponse = $contextIO->listConnectTokens(); 
-	var_dump($listAccountTokensResponse); 
-}
+	//This function should clear out all tokens and accounts associated with our keys. 
+	function flushTokens()
+	{
+		$contextIO = new ContextIO('qd8cq03s','SogN0NW6RPJPkStv');
+		$listAccountTokensResponse = $contextIO->listConnectTokens(); 
+		//var_dump($listAccountTokensResponse); 
+		$accountListResponse = $contextIO->listAccounts(); 
+	}
 
-//This makes the URL for connecting and doing OAuth
-function connect()
-{
-	$contextIO = new ContextIO('qd8cq03s','SogN0NW6RPJPkStv');
-	$addTokenResponse = $contextIO->addConnectToken(array('callback_url' => 'http://tnevets.net/LAHacks/'));
-	//get the redirect url from the response, and direct the user to it
-	$redirectUrl = $addTokenResponse->getDataProperty('browser_redirect_url');
-	return $redirectUrl; 
-}
-
+	//This makes the URL for connecting and doing OAuth
+	function connect()
+	{
+		$contextIO = new ContextIO('qd8cq03s','SogN0NW6RPJPkStv');
+		$addTokenResponse = $contextIO->addConnectToken(array('callback_url' => 'http://parsly.rocks/resources/index.php'));
+		//get the redirect url from the response, and direct the user to it
+		$redirectUrl = $addTokenResponse->getDataProperty('browser_redirect_url');
+		return $redirectUrl; 
+	}
+	//Gets 50 emails once you log in!	
+	function getEmails($emailID)
+	{
+	$response = array(); 
+	$contextIO = new ContextIO('qd8cq03s','SogN0NW6RPJPkStv');	
+	$messageListResponse = $contextIO->listMessages($emailID,array('label' => '0', 'folder' => 'Inbox', 'limit' => '50'));
+	foreach($messageListResponse->getData() as $message)
+	{
+		array_push($response,array($message['subject'],$message['message_id'])); 
+	}  
+	return $response; 
+	}
 ?>
