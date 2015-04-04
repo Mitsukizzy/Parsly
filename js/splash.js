84f102849289b156749c82c337664bbec327a62a
Izzy Benavente <ibenaven@usc.edu>
2015-04-04 14:11:55 -0700

    Added splash screen

---
+++ js/splash.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    $('#logo').fadeIn(3000, function(){
+        $('#signup').slideDown(1500);
+    });
+});
---
+++ splash.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Izzy on 4/4/2015.
+ */
